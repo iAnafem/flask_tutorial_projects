6a4f44506832fb3889749745a3f2269125881b20
Пронькин Дмитрий Юрьевич <DPronkin@mostro.ru>
2019-08-14 15:19:44 +0300

    microblog: part 14 complete

--- microblog/config.py
+++ microblog/config.py
@@ -16,3 +16,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 10
+    LANGUAGES = ['ru']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
