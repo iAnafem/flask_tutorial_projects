17696967644f56aae3e10a2bc4ff8dfd2b0ea1d5
iAnafem <sidviciouc@gmail.com>
2019-08-15 23:24:21 +0300

    microblog: text search completed

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -10,7 +10,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_babel import Babel, lazy_gettext as _l
 from config import Config
-from elasticsearch import Elasticsearch
+from elasticsearch6 import Elasticsearch
 
 
 db = SQLAlchemy()
